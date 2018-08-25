f4a7afd19db83990489cd18325b75273bddd8285
EnriqueLanderosOntiveros <A01336571@itesm.mx>
2018-08-24 22:43:35 -0500

    unity-sessions-interaction-between-objects-setup

---
+++ interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/GameMaster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeChangesPlayerLife
+{
+	public class GameMaster : MonoBehaviour {
+        public Sphere Sphere;
+        // Use this for initialization
+        void OnMouseDown()
+        {
+         
+        
+            Sphere.ReduceLife();
+        }
+
+        void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
+}
---
+++ interaction-between-gameobjects/Assets/Scripts/CubeChangesPlayerLife/Sphere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+    public int Lifepoints = 100;
+    public Texture texto;
+     void OnGUI()
+    {
+        GUILayout.Label(texto);
+        GUILayout.Label("Tienes " + Lifepoints + "Lifepoints;");
+    }
+    public void ReduceLife()
+    {
+        Lifepoints = Lifepoints - 1;
+    }
+    void OnMouseDown()
+    {
+        this.ReduceLife();   
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
